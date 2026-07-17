5f548f75cf44d5d03d3a54bff3fa69ac1c14f602
dflail <dan.flail.dev@gmail.com>
2026-07-17 02:57:56 -0400

    Open Router and main.py updates

--- main.py
+++ main.py
@@ -1,6 +1,29 @@
-def main():
-    print("Hello from aigent!")
+import os
 
+from dotenv import load_dotenv
+from openai import OpenAI
+
+def main() -> None:
+    load_dotenv()
+    api_key = os.environ.get("OPENROUTER_API_KEY")
+
+    if not api_key:
+        raise RuntimeError("OPENROUTER_API_KEY environment variable is not set. Please set it in your .env file.")
+
+    client = OpenAI(
+        base_url="https://openrouter.ai/api/v1",
+        api_key=api_key
+    )
+    response = client.chat.completions.create(
+        model="openrouter/free",
+        messages=[
+            {
+                "role": "user",
+                "content": "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.",
+            }
+        ],
+    )
+    print(f"Response: {response.choices[0].message.content}")
 
 if __name__ == "__main__":
     main()
